9401c6377b7c32db85e8e1848b260d2f7b2db9ca
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-05-30 00:50:36 +0300

    forgot to return the promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
       const [response, user] = data;
       console.log(`${response.body} ${user.firstName} ${user.lastName}`);
