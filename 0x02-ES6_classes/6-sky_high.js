6de5c96f6a4fc17bd3d9f586bdc679a11cb21159
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-05-31 00:05:18 +0300

    6. Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,26 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  /**
+   * @param {int} value
+   */
+  set floors(value) {
+    if (!Number.isInteger(value)) {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
