95a18dcf3f2eb4ee9bee9c6527524727755621c8
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-05-30 23:16:45 +0300

    3. Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  /**
+   * @param {String} value
+   */
+  set code(value) {
+    if (!(typeof value === 'string')) {
+      throw new TypeError('code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  /**
+   * @param {string} value
+   */
+  set name(value) {
+    if (!(typeof value === 'string')) {
+      throw new TypeError('name must be a string');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
