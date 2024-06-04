158da5e11e3d50d4d9847f32bdd9988a4da9808c
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-04 22:37:03 +0000

    Check set with instanceof instead of set.constructor.name

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const matchArray = [];
-  if (typeof startString !== 'string' || startString === '' || set.constructor.name !== 'Set') {
+  if (typeof startString !== 'string' || startString === '' || !(set instanceof Set)) {
     return '';
   }
   set.forEach((value) => {
