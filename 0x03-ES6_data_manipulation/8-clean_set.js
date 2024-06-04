150bddeb9fc2df6c8a6162d4a9019e626a83802e
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-04 22:42:12 +0000

    Check if items in the set is strings

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,7 @@ export default function cleanSet(set, startString) {
     return '';
   }
   set.forEach((value) => {
-    if (value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       matchArray.push(value.slice(startString.length));
     }
   });
