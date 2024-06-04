97d92979c817f215965085e8ea5458464155c110
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-04 23:38:51 +0000

    Refactor

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,9 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (weakMap.has(endpoint)) {
-    const counter = weakMap.get(endpoint);
-    if (counter >= 4) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, (weakMap.get(endpoint) || 0) + 1);
-  } else {
-    weakMap.set(endpoint, 1);
+  weakMap.set(endpoint, (weakMap.get(endpoint) || 0) + 1);
+  const counter = weakMap.get(endpoint);
+  if (counter >= 5) {
+    throw new Error('Endpoint load is high');
   }
 }
