43d7a775c5207b410d0056ff704904a765f22487
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-04 23:25:13 +0000

    check if endpoint have name & protocol

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,13 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const counter = weakMap.get(endpoint);
-  if (counter >= 5) {
-    throw new Error('Endpoint load is high');
+  const objectProperties = Object.getOwnPropertyNames(endpoint);
+
+  if (objectProperties.includes('name') && objectProperties.includes('protocol')) {
+    const counter = weakMap.get(endpoint);
+    if (counter >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, (counter || 0) + 1);
   }
-  weakMap.set(endpoint, (weakMap.get(endpoint) || 0) + 1);
 }
