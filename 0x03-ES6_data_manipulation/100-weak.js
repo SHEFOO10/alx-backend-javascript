a8fb6107cfca74c01b7231e6b2d13129ea110348
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-04 23:30:35 +0000

    handle if endpoint is not exist in the weakMap and Got a query

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     const counter = weakMap.get(endpoint);
-    if (counter >= 5) {
+    if (counter >= 4) {
       throw new Error('Endpoint load is high');
     }
     weakMap.set(endpoint, (weakMap.get(endpoint) || 0) + 1);
