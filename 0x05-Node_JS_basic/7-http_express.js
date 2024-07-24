1a35ae8677cbf648b1a6f161641bceb3513cc04f
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-24 00:03:27 +0000

    7. Create a more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const fs = require('fs');
+
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
+
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      if (lines.length === 0) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const headdings = lines[0].split(',');
+      const students = lines.slice(1).map((line) => line.split(','));
+
+      console.log(`Number of students: ${students.length}`);
+
+      const fieldCounts = {};
+      const fieldNames = {};
+      students.forEach((student) => {
+        if (student.length !== headdings.length) return;
+        const field = student[3];
+        if (!fieldCounts[field]) {
+          fieldCounts[field] = 0;
+          fieldNames[field] = [];
+        }
+        fieldCounts[field] += 1;
+        fieldNames[field].push(student[0]);
+      });
+
+      Object.keys(fieldCounts).forEach((field) => {
+        console.log(`Number of students in ${field}: ${fieldCounts[field]}. List: ${fieldNames[field].join(', ')}`);
+      });
+
+      resolve();
+    });
+  });
+}
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+const output = [];
+app.get('/students', (req, res) => {
+  output.unshift('This is the list of our students\n');
+  res.send(output.join(''));
+});
+
+app.listen(1245);
+const originalstdoutWrite = process.stdout.write;
+
+process.stdout.write = (data) => {
+  output.push(data);
+};
+countStudents(DB_FILE).then(() => {
+  process.stdout.write = originalstdoutWrite;
+}).catch((err) => {
+  output.push(err.message);
+  process.stdout.write = originalstdoutWrite;
+});
+
+module.exports = app;
