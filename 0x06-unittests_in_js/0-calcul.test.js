bd441da04f5fffa901a0011c86ca745ed0e5dee1
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-25 12:22:15 +0000

    0. Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+var assert = require('assert')
+const calculateNumber = require('./0-calcul')
+
+describe('calculateNumber', function () {
+  it('Add int to int', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('Add int to float', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('Add float to float : Rounded', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('Add float to float : Rounded', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+});
