34f5108a513f81fcc20673646c1d05b39231df38
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-25 13:33:30 +0000

    change test messages and add new testcase

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,16 +2,19 @@ var assert = require('assert')
 const calculateNumber = require('./0-calcul')
 
 describe('calculateNumber', function () {
-  it('add int to int', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+  it('floating point whole numbers', function() {
+    assert.strictEqual(calculateNumber(1.0, 3.0), 4);
   });
-  it('add int to float', function() {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it('rounding up b\'s floating point fractional number', function() {
+    assert.strictEqual(calculateNumber(1.0, 3.7), 5);
   });
-  it('add float to float : Rounded 1', function() {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it('rounding down a, b\'s floating point fractional numbers', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.3), 4);
   });
   it('add float to float : Rounded 2', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+  it('rounding downin a, b floating point fractional numbers with tralling 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.4999999, 4.499999999), 6)
+  })
 });
