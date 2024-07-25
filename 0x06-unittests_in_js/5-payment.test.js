3f2028bee684800b6c509f55dbb513020ae2ae0f
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-07-25 21:27:09 +0000

    5. Hooks

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -4,20 +4,22 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 
 describe('sendPaymentRequestToApi', () => {
+  let logSpy;
+  beforeEach(() => {
+    logSpy = sinon.spy(console, 'log');
+  });
+  afterEach(() => {
+	  logSpy.restore();
+  });
+
   it('should call calculateNumber with "SUM", 100, and 20', () => {
-    // Create a spy for the log
-    const logSpy = sinon.spy(console, 'log');
     // Call the function to test
     const result = sendPaymentRequestToApi(100, 20);
 
     // Verify that log was called with the correct message
     expect(logSpy.calledWith('The total is: 120')).to.be.true;
-    // Restore the original function
-    logSpy.restore();
   });
   it('should call calculateNumber with "SUM", 10, and 10', () => {
-        // Create a spy for the log
-	  const logSpy = sinon.spy(console, 'log');
 	  // Call the function to test
 	  const result = sendPaymentRequestToApi(10, 10);
 	  // Verify that log was called with the correct message
