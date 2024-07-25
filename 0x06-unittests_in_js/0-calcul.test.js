266899197b809e79ac1f6d1523430b99aacdcd86
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-25 12:54:22 +0000

    updated

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,10 +8,10 @@ describe('calculateNumber', function () {
   it('Add int to float', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
-  it('Add float to float : Rounded', () => {
+  it('Add float to float : Rounded 1', () => {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it('Add float to float : Rounded', () => {
+  it('Add float to float : Rounded 2', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
