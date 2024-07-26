59199882fdfeca346ea6d27612080cc5d06b8b2b
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-07-26 16:06:57 +0000

    forgot to export app & load database path from argv when route called

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,7 +3,8 @@ import readDatabase from '../utils'
 class StudentsController {
   static getAllStudents(request, response) {
     const responseText = ['This is the list of our students'];
-    readDatabase(request.DB_FILE)
+    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+    readDatabase(dataPath)
       .then((res) => {
 	responseText.push(...res.responseText)
         response.send(responseText.join('\n'));
@@ -16,7 +17,8 @@ class StudentsController {
   static getAllStudentsByMajor(request, response) {
     const majors = ['CS', 'SWE'];
     if (!majors.includes(request.params.major)) response.status(500).send('Major parameter must be CS or SWE');
-    readDatabase(request.DB_FILE)
+    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+    readDatabase(dataPath)
       .then((data) => {
         response.send(`List: ${data.fieldNames[request.params.major].join(', ')}`);
       })
@@ -29,3 +31,4 @@ class StudentsController {
 }
 
 export default StudentsController;
+module.exports = StudentsController;
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -2,13 +2,10 @@ import express from 'express'
 import routes from './routes/index'
 
 const app = express();
-const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
-
-app.use((req, res, next) => {
-  req.DB_FILE = DB_FILE;
-  next();
-});
 
 app.use(routes);
 
 app.listen(1245, () => console.log('Server running on port 1245'));
+
+export default app;
+module.exports = app;
